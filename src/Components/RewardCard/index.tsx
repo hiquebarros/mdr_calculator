f0e0392af4b3736d628079e6e01375ca20181328
Henrique Barros <henrique1_barros@hotmail.com>
2022-09-28 16:36:38 -0300

    feat: days funcionality

--- src/Components/RewardCard/index.tsx
+++ src/Components/RewardCard/index.tsx
@@ -3,13 +3,14 @@ import { Container } from "./styles";
 interface IRewardCardProps {
     date: string
     value: number
+    response: boolean
 }
 
-const RewardCard = ({date, value}: IRewardCardProps) => {
+const RewardCard = ({date, value, response}: IRewardCardProps) => {
     return (
         <Container>
             <h3>{date}:</h3>
-            <h3>R$ {value && value.toString()},00</h3>
+            {response ? (<h3> R$ {value && value.toString()},00</h3>) : (null)}
         </Container>
     );
 }
