d22514129197eb4218f75fe30e73bc46e7b1d2a5
Henrique Barros <henrique1_barros@hotmail.com>
2022-09-30 19:03:33 -0300

    feat: tests

--- src/Components/RewardCard/index.tsx
+++ src/Components/RewardCard/index.tsx
@@ -1,14 +1,15 @@
 import { Container } from "./styles";
 
 interface IRewardCardProps {
+    id: number
     date: string
     value: number
     response: boolean
 }
 
-const RewardCard = ({date, value, response}: IRewardCardProps) => {
+const RewardCard = ({id, date, value, response}: IRewardCardProps) => {
     return (
-        <Container>
+        <Container data-testid={id}>
             <h3 style={{marginRight: "10px"}}>{date}</h3>
             <h3></h3>{response ? (<h3> R$ {value && (value / 100).toFixed(2).toString().replace('.', ",")}</h3>) : (null)}
         </Container>
